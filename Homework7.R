f013bcb5a6dae4c1b0bba174c4131737a700dc4d
vicmermon <victor.merino.montes@student.uva.nl>
2023-03-01 22:47:11 +0100

    1 March Workshop update + Changes to plot

--- Homework7.R
+++ Homework7.R
@@ -1,35 +1,33 @@
 library(tidyverse)
 library(spotifyr)
+library(plotly)
 
 # Track info from the tracks in the Hyperpop playlist
 hyper <- get_playlist_audio_features("", "37i9dQZF1DX7HOk71GPfSw")
 classic <- get_playlist_audio_features("", "37i9dQZF1DXakZCf7SV1CS")
 sleepy <- get_playlist_audio_features("", "1darBgHtPFeoXVS1qvXhUD")
+perfect <- get_playlist_audio_features("", "6oaHLB9qwLOo0RVpU2gGJ5")
 
 corpus <-
   bind_rows(
     hyper |> mutate(category = "Hyperpop"),
-    classic |> mutate(category = "Hyperpop Classic"),
+    classic |> mutate(category = "Hyperpop Classics"),
     sleepy |> mutate(category = "Sleepy Zone"),
+    perfect |> mutate(category = "Perfect Music Friday"),
   )
 
-corpus |>
-  ggplot(aes(x = valence, y = energy, size = tempo, color = category)) +
-  geom_jitter(alpha = 0.8,) +
-  facet_wrap(~category) +
+  ggplot(corpus, aes(x = tempo, y = energy, size = valence, color = category )) +
+  geom_jitter() +
+  
   scale_color_brewer(
     type = "qual",
     palette = "Spectral",
-    guide = "none",
-  ) +
-  scale_size_continuous(
-    trans = "reverse",
-    labels = waiver(),
-    name ="BPM"
-
-  )
-
-install.packages("flexdashboard")
+    name = "Playlist"
+  
+  ) + ggtitle("Common characteristics in hyperpop songs")  +
  
+    + scale_fill_viridis_c() + theme_classic() +
+
+ggplotly()
  
 
---
+++ workshop1march.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(spotifyr)
+library(compmus)
+
+bzt <-
+  get_tidy_audio_analysis("2Gt94qSL9EImDLGm6Jlu3S") |> # Change URI.
+  compmus_align(bars, segments) |>                     # Change `bars`
+  select(bars) |>                                      #   in all three
+  unnest(bars) |>                                      #   of these lines.
+  mutate(
+    pitches =
+      map(segments,
+          compmus_summarise, pitches,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  ) |>
+  mutate(
+    timbre =
+      map(segments,
+          compmus_summarise, timbre,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  )
+
+bzt |>
+  compmus_self_similarity(timbre, "cosine") |> 
+  ggplot(
+    aes(
+      x = xstart + xduration / 2,
+      width = xduration,
+      y = ystart + yduration / 2,
+      height = yduration,
+      fill = d
+    )
+  ) +
+  geom_tile() +
+  coord_fixed() +
+  scale_fill_viridis_c(guide = "none") +
+  theme_classic() +
+  labs(x = "", y = "")
